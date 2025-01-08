f6df9f7608d4c6a3902d75664c6720ab428a5dfa
afeeda <apafeeda@gmail.com>
2025-01-08 11:29:16 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+//infer types(implicit types)
+
+// let username="afeeda";
+// username=0;
+// let subcribers=30_000;
+// subcribers="addddd";
+
+//defining types (explicit types)
+let username:string="debug";
+let subcribers:number=30_000;
+let issubscribed:boolean=true;
+let skills:string[]=["js","react","ts"];
+let count:number[]=[1,2,3,4]
+let emptystring:[]=[]
